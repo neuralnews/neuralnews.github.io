2935db65adc199c0070cacab37d657c38d1c59e5
Brendan Voelz <bvoelz19@gmail.com>
2017-09-16 11:45:03 -0400

    updates to leg 1

--- NeuralNews/ios/NeuralNewsTests/NeuralNewsTests.m
+++ NeuralNews/ios/NeuralNewsTests/NeuralNewsTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface NeuralNewsTests : XCTestCase
+@interface neuralnewsTests : XCTestCase
 
 @end
 
-@implementation NeuralNewsTests
+@implementation neuralnewsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
