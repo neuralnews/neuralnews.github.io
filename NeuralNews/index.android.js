c9e44b292f3ff471fa003e98cf9c805109ca4b47
Brendan Voelz <bvoelz19@gmail.com>
2017-10-01 18:39:47 -0400

    fresh project without typescript

--- NeuralNews/index.android.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {
-  AppRegistry
-} from 'react-native';
-import App from './build'
-const rntypescript = () => <App />
-AppRegistry.registerComponent('neuralnews', () => rntypescript);
