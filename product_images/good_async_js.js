4472b854b98d85f43b17dcd4a2abb143708f97bf
Brendan Voelz <bvoelz19@gmail.com>
2017-10-31 13:54:31 -0400

    new entry

--- product_images/good_async_js.js
+++ product_images/good_async_js.js
@@ -1,4 +1,4 @@
-var articles  = getArticles();
+var deferred = Q.defer();
 var json_data = '';
 
 for (var i = 0; i < articles.length; i++) {
@@ -6,7 +6,10 @@ for (var i = 0; i < articles.length; i++) {
         // Asynchronous process to perform NLP analysis
         // on each article and return the results as JSON string
         json_data += processArticle(articles[i]);
+
+        if (finishedProcessingArticles()) {
+                deferred.resolve(json_data);
+        }
 }
 
-return json_data;
-
+return deferred.promise;
